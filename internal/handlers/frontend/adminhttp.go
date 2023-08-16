104c6ed4d39f23c819597f68904c8eb8875afa6c
sembh1998 <sembenavente@gmail.com>
2023-08-15 22:51:47 -0500

    feat: :zap: limit of 10 attemps per ip for login, then freeze 5 min

--- internal/handlers/frontend/adminhttp.go
+++ internal/handlers/frontend/adminhttp.go
@@ -4,6 +4,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sembh1998/wedding_invitation_generation/internal/core/ports"
@@ -30,6 +32,21 @@ type LoginRequest struct {
 	Password string `json:"password" binding:"required" form:"password"`
 }
 
+type LoginTracker struct {
+	attempts   map[string]int
+	lastFailed map[string]time.Time
+	mutex      sync.Mutex
+	timeout    time.Duration
+	maxFailed  int
+}
+
+var loginTracker = LoginTracker{
+	attempts:   make(map[string]int),
+	lastFailed: make(map[string]time.Time),
+	timeout:    5 * time.Minute,
+	maxFailed:  10,
+}
+
 func (h *HTTPHandler) Login(c *gin.Context) {
 	req := LoginRequest{}
 	if err := c.ShouldBind(&req); err != nil {
@@ -37,15 +54,47 @@ func (h *HTTPHandler) Login(c *gin.Context) {
 		return
 	}
 
+	ip := c.ClientIP()
+
+	loginTracker.mutex.Lock()
+	defer loginTracker.mutex.Unlock()
+
+	// Check if the IP has a recent failed attempt
+	lastFailedTime, exists := loginTracker.lastFailed[ip]
+	if exists && time.Since(lastFailedTime) < loginTracker.timeout {
+		log.Printf("Login failed for user %s from IP %s. Too many attempts. Remaining time: %v", req.Username, ip, loginTracker.timeout-time.Since(lastFailedTime))
+		c.JSON(http.StatusTooManyRequests, gin.H{"error": "Too many login attempts. Please try again later."})
+		return
+	}
+
+	if exists && time.Since(lastFailedTime) >= loginTracker.timeout {
+		log.Printf("Time since last failed login for user %s from IP %s is %v. Resetting.", req.Username, ip, time.Since(lastFailedTime).String())
+		delete(loginTracker.lastFailed, ip)
+		delete(loginTracker.attempts, ip)
+	}
+
+	attempts, exists := loginTracker.attempts[ip]
+	if exists && attempts >= loginTracker.maxFailed {
+		loginTracker.lastFailed[ip] = time.Now()
+		log.Printf("Max failed login attempts reached for user %s from IP %s. Blocking for %v", req.Username, ip, loginTracker.timeout.String())
+		c.JSON(http.StatusTooManyRequests, gin.H{"error": "Too many login attempts. Please try again later."})
+		return
+	}
+
 	token, err := h.userSrv.Login(req.Username, req.Password)
 	if err != nil {
+		attempts++
+		loginTracker.attempts[ip] = attempts
+		log.Printf("Login failed for user %s from IP %s. %d attempts so far.", req.Username, ip, attempts)
 		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 		return
 	}
+
+	delete(loginTracker.attempts, ip)
+	delete(loginTracker.lastFailed, ip)
+
 	domain := c.Request.Host
 	log.Println(domain)
 	c.SetCookie("wedding_cookie", token, 3600*24, "/", domain, false, true)
-	// relocate to /crudguests
-	// Set HX-Redirect header for successful login
 	c.Header("HX-Redirect", "/crudguests")
 }
