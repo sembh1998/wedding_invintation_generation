f68215dc746ce7211470a1c64f6f4e7b442d28b0
sembh1998 <sembenavente@gmail.com>
2023-08-23 10:25:32 -0500

    feat: :sparkles: AttendanceLimit added to Guest Entity

--- internal/core/domain/guests.go
+++ internal/core/domain/guests.go
@@ -2,11 +2,12 @@ package domain
 
 type Guest struct {
 	BaseModel
-	Name           string `form:"name"`
-	LastName       string `form:"last_name"`
-	WillAttend     int    `form:"will_attend"`
-	SpecialMessage string `gorm:"type:text" form:"special_message"`
-	Response       string `gorm:"type:text" form:"response"`
+	Name            string `form:"name"`
+	LastName        string `form:"last_name"`
+	WillAttend      int    `form:"will_attend"`
+	SpecialMessage  string `gorm:"type:text" form:"special_message"`
+	Response        string `gorm:"type:text" form:"response"`
+	AttendanceLimit int    `form:"attendance_limit"`
 	// foriegn key to user
 	CreatedBy string
 	User      User `gorm:"foreignKey:CreatedBy"`
